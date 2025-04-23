820531113ca199c3378c117454ba7ebb8636b8be
Rudra Das <dasrudra234@gmail.com>
2025-04-23 14:26:12 +0530

    lab

--- rudra/chatserverTCP.c
+++ rudra/chatserverTCP.c
@@ -10,21 +10,7 @@
 int main() {
     WSADATA wsa;
     SOCKET server_socket, client_socket;
-    /**
-     * Struct declarations for server and client socket addresses.
-     * 
-     * - `server_addr`: This structure holds the address information for the server.
-     *   It is used to specify the IP address and port number on which the server
-     *   will listen for incoming connections.
-     * 
-     * - `client_addr`: This structure holds the address information for the client.
-     *   It is used to store the IP address and port number of the client that
-     *   connects to the server. This is typically populated when a connection
-     *   request is accepted by the server.
-     * 
-     * Both structures are of type `struct sockaddr_in`, which is specifically
-     * designed for handling IPv4 addresses in socket programming.
-     */
+    
     struct sockaddr_in server_addr, client_addr;
     int client_addr_len = sizeof(client_addr);
     char buffer[BUFFER_SIZE];
